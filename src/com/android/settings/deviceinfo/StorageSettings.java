7232e3705e99a6ab8767dd1a057da9970e490d25
Phaninder G <gattu.phani88@yahoo.com>
2018-03-23 12:36:07 +0530

    Update StorageSettings.java
    missed few changes

--- src/com/android/settings/deviceinfo/StorageSettings.java
+++ src/com/android/settings/deviceinfo/StorageSettings.java
@@ -292,6 +292,9 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
             }
 
             if (vol.getType() == VolumeInfo.TYPE_PRIVATE) {
+                final List<VolumeInfo> volumes = mStorageManager.getVolumes();
+                long primaryPhysicalTotalSpace =
+                        PrivateStorageInfo.getPrimaryPhysicalTotalSpace(volumes);
                 final Bundle args = new Bundle();
                 args.putString(VolumeInfo.EXTRA_VOLUME_ID, vol.getId());
                 PrivateVolumeSettings.setVolumeSize(args, PrivateStorageInfo.getTotalSize(vol,
